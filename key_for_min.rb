11b675f5a79a5ab3f5ffc7a2e15f608d3c12f7e8
Cara Zellner <carajeanine@gmail.com>
2020-06-03 20:01:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,15 @@
 def key_for_min_value(name_hash)
  lowest_key = nil 
  lowest_value = nil
- hash.each do |key, value|
-   if lowest_value == nil || value < lowest_value
+ name_hash.each do |key, value|
+   if lowest_value == nil
+     lowest_value = value
+     lowest_key = key
+   elsif lowest_value > value
      lowest_value = value
      lowest_key = key
    end
   end
   lowest_key
 end
-key_for_min_value(name_hash)
+lowest_key
